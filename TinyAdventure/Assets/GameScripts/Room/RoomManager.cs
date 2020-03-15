e24feb2e32a85d54024a69fcbc0759b1cbe846da
anyue <1183767577@qq.com>
2020-03-15 19:33:18 +0800

    导入资源。

---
+++ TinyAdventure/Assets/GameScripts/Room/RoomManager.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-03-15 16:16:12 
+ * @Description: 房间管理器
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-03-15 16:16:36
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+
+}
