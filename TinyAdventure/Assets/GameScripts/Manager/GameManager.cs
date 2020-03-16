e85ef055bb96a5fefe5ce094786c84e0c08c6d91
anyue <1183767577@qq.com>
2020-03-16 22:07:06 +0800

    导入素材。

---
+++ TinyAdventure/Assets/GameScripts/Manager/GameManager.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-03-15 21:00:27 
+ * @Description: 游戏管理器
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-03-15 21:01:24
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.getInstance().showBoard("UGameBoard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TinyAdventure/Assets/GameScripts/UI/Board/UGameBoard.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-03-15 20:57:37 
+ * @Description: 游戏界面ui
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-03-15 20:57:59
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UGameBoard : BaseUI
+{
+    
+}
