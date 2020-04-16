314dadb869feda68f7da1f804f170a9e7692b684
anyue <1183767577@qq.com>
2020-04-16 23:00:03 +0800

    添加动画。

--- TinyAdventure/Assets/GameScripts/Manager/GameManager.cs
+++ TinyAdventure/Assets/GameScripts/Manager/GameManager.cs
@@ -10,17 +10,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        UIManager.getInstance().showBoard("UGameBoard");
-    }
+    void Start () { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 }
