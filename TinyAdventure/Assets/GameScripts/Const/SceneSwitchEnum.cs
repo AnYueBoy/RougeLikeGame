2493ff36aa8d45882611548473431f7da739860d
anyue <1183767577@qq.com>
2020-05-05 17:13:47 +0800

    添加过场效果。

---
+++ TinyAdventure/Assets/GameScripts/Const/SceneSwitchEnum.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2020-05-05 16:38:38 
+ * @Description: 场景切换动画枚举  
+ * @Last Modified by: l hy
+ * @Last Modified time: 2020-05-05 16:53:33
+ */
+
+public enum SceneSwitchEnum {
+
+    FADE,
+    CIRCLE,
+    LOGO,
+}
--- TinyAdventure/Assets/GameScripts/Manager/Appcontext.cs
+++ TinyAdventure/Assets/GameScripts/Manager/Appcontext.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 /*
  * @Author: l hy 
  * @Date: 2020-05-04 19:19:39 
  * @Description: 全局appcontext
  * @Last Modified by: l hy
- * @Last Modified time: 2020-05-04 19:29:50
+ * @Last Modified time: 2020-05-05 17:12:48
  */
 
 public class Appcontext : MonoBehaviour {
@@ -14,20 +15,26 @@ public class Appcontext : MonoBehaviour {
         return m_Instance;
     }
 
-    public InputManager m_inputManager = null;
+    private InputManager m_inputManager = null;
     public InputManager inputManager {
         get {
             return this.m_inputManager;
         }
     }
 
-    public PlayerManager m_playerManager = null;
+    private PlayerManager m_playerManager = null;
     public PlayerManager playerManager {
         get {
 
             return this.m_playerManager;
         }
     }
+
+    [Header ("淡入淡出切换动画管理器")]
+    public Animator fadeSceneAnimator = null;
+
+    [Header ("圆形动画管理器")]
+    public Animator circleAnimator = null;
 
     private void Awake () {
         m_Instance = this;
@@ -47,6 +54,40 @@ public class Appcontext : MonoBehaviour {
         } catch (System.Exception) {
             Debug.Log ("player manager set fail");
         }
+    }
+
+    public void loadNextScene (SceneSwitchEnum sceneType) {
+
+        switch (sceneType) {
+
+            case SceneSwitchEnum.FADE:
+                this.fadeSceneAnimator.transform.parent.gameObject.SetActive (true);
+                this.circleAnimator.transform.parent.gameObject.SetActive (false);
+                // TODO: LOGO
+                StartCoroutine (animationLoad (this.fadeSceneAnimator));
+                break;
+
+            case SceneSwitchEnum.CIRCLE:
+                this.fadeSceneAnimator.transform.parent.gameObject.SetActive (false);
+                this.circleAnimator.transform.parent.gameObject.SetActive (true);
+                StartCoroutine (animationLoad (this.circleAnimator));
+                break;
+
+            case SceneSwitchEnum.LOGO:
+                break;
+
+            default:
+                Debug.LogWarning ("scene switch enum is not exist" + sceneType);
+                break;
+        }
+    }
+
+    IEnumerator animationLoad (Animator targetAnimator) {
+        targetAnimator.SetTrigger ("Start");
+
+        yield return new WaitForSeconds (1);
+
+        SceneLoadManager.getInstance ().loadNextScene ();
     }
 
 }
